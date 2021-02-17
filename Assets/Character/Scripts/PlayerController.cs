42b316261f71702b8ed1b4f2fee5e78f540d4ce0
Tianwei <s3702768@student.rmit.edu.au>
2021-02-18 00:22:01 +1100

    finished basic movement

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -6,23 +6,28 @@ public class PlayerController : MonoBehaviour{
 
     public float moveSpeed;
     public float jumpForce;
+    public bool grounded;
+    public LayerMask whatIsGround;
 
     private Rigidbody2D myRigidbody;
+    private Collider2D myCollider;
 
     // Use this for initialization
     void Start(){
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update(){
 
-        // Move to right automatically
-        myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y);
+        // Move forward automatically
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
-        // Press Space to jump
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouse){
-
+        // Press Space to jump only when player is on the ground
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && grounded){
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
         }
 
     }
