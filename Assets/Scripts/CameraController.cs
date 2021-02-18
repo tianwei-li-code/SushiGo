15b57efa2b5a6ad02fd3cd8f1bd086055854d794
Tianwei <s3702768@student.rmit.edu.au>
2021-02-18 18:35:48 +1100

    finished animation and dust particle effect

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+
+    public PlayerController player;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+    // Start is called before the first frame update
+    void Start(){
+        player = FindObjectOfType<PlayerController>();
+        lastPlayerPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        distanceToMove = player.transform.position.x - lastPlayerPosition.x;
+        transform.position = new Vector3(transform.position.x+distanceToMove,transform.position.y,transform.position.z);
+        lastPlayerPosition = player.transform.position;
+    }
+}
