063c27cf1c557254c82ff37faf7d9096e63b2d88
Tianwei <s3702768@student.rmit.edu.au>
2021-02-19 16:11:43 +1100

    finished random platform generation

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour{
 
-    public GameObject platform;
     public Transform generationPoint;
-    public ObjectPooler objectPool;
+    public ObjectPooler[] objectPools;
 
     private float platformWidth;
+    private int platformSelector;
 
     // Start is called before the first frame update
     void Start(){
-        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+        platformWidth = objectPools[0].pooledObject.GetComponent<BoxCollider2D>().size.x;
     }
 
     // Update is called once per frame
     void Update(){
         if(transform.position.x < generationPoint.position.x){
             transform.position = new Vector3(transform.position.x + platformWidth, transform.position.y, transform.position.z);
-            GameObject newPlatform = objectPool.GetPooledObject();
+            platformSelector = Random.Range(0,objectPools.Length);
+
+            GameObject newPlatform = objectPools[platformSelector].GetPooledObject();
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
             newPlatform.SetActive(true);
