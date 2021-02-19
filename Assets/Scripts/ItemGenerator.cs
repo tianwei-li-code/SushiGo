3c4d5db3ef116a32ab2ba22f3d6e5c858fa74c35
Tianwei <s3702768@student.rmit.edu.au>
2021-02-19 18:27:55 +1100

    finished item generator

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour{
+    // Start is called before the first frame update
+    public Transform generationPoint;
+    public ObjectPooler[] objectPools;
+    public float minDistance;
+    public float maxDistance;
+    public float distance;
+
+    private int itemSelector;
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(transform.position.x < generationPoint.position.x){
+            distance = Random.Range(minDistance, maxDistance);
+
+            itemSelector = Random.Range(0, objectPools.Length);
+            
+            transform.position = new Vector3(transform.position.x + distance, transform.position.y, 
+                                            transform.position.z);
+            
+            GameObject newPlatform = objectPools[itemSelector].GetPooledObject();
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -19,7 +19,7 @@ public class PlatformGenerator : MonoBehaviour{
     void Update(){
         if(transform.position.x < generationPoint.position.x){
             transform.position = new Vector3(transform.position.x + platformWidth, transform.position.y, transform.position.z);
-            platformSelector = Random.Range(0,objectPools.Length);
+            platformSelector = Random.Range(0, objectPools.Length);
 
             GameObject newPlatform = objectPools[platformSelector].GetPooledObject();
             newPlatform.transform.position = transform.position;
