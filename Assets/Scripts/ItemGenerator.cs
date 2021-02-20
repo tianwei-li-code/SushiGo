b0d6fb00c1adeee22c114bb6c91b6337931f637f
Tianwei <s3702768@student.rmit.edu.au>
2021-02-20 20:46:00 +1100

    finished random height genration for sushi, milestone 1 completed

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -8,23 +8,34 @@ public class ItemGenerator : MonoBehaviour{
     public ObjectPooler[] objectPools;
     public float minDistance;
     public float maxDistance;
+    public Transform maxHeightPoint;
 
     private int itemSelector;
     private float distance;
-    void Start(){
+    private float minHeight;
+    private float maxHeight;
+    private float newHeight;
 
+    void Start(){
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
     }
 
     // Update is called once per frame
     void Update(){
         if(transform.position.x < generationPoint.position.x){
+
+            // Generate random distance between two items
             distance = Random.Range(minDistance, maxDistance);
 
+            // Choose random items from objectPools
             itemSelector = Random.Range(0, objectPools.Length);
+
+            // If item's tag is Sushi, generate random height
+            newHeight = objectPools[itemSelector].tag == "Sushi" ? Random.Range(minHeight, maxHeight) : minHeight;
+            transform.position = new Vector3(transform.position.x + distance, newHeight, transform.position.z);
             
-            transform.position = new Vector3(transform.position.x + distance, transform.position.y, 
-                                            transform.position.z);
-            
+            // Generate the item
             GameObject newItem = objectPools[itemSelector].GetPooledObject();
             newItem.transform.position = transform.position;
             newItem.transform.rotation = transform.rotation;
